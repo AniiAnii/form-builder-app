0e040df9530250fcfcab3ce1284d3487ef053497
Anica Arsić <anica.arsic@dct.rs>
2025-08-31 03:54:39 +0200

    Add API and authService for backend communication

---
+++ frontend/src/services/api.js
@@ -0,0 +1,10 @@
+// frontend/src/services/api.js
+import axios from "axios";
+
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+
+const api = axios.create({
+  baseURL: `${API_BASE}/api`,
+});
+
+export default api;
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,19 @@
+// frontend/src/services/authService.js
+import api from "./api";
+
+export const register = async ({ name, email, password }) => {
+  const res = await api.post("/auth/register", { name, email, password });
+  return res.data;
+};
+
+export const login = async ({ email, password }) => {
+  const res = await api.post("/auth/login", { email, password });
+  return res.data;
+};
+
+export const me = async (token) => {
+  const res = await api.get("/auth/me", {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return res.data;
+};
