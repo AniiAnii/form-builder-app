98a4c3ee46f0c8e3f4e84816869cbcee4c6bf36d
cokahaha <andrijanaa.mihailovic@gmail.com>
2025-09-18 14:07:33 +0200

    Temp: save local changes before pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Dashboard from "./pages/Dashboard";
 import CreateFormPage from "./pages/CreateFormPage";
 
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* Public Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Auth Pages */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Pages (you'll add auth later) */}
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/forms/create" element={<CreateFormPage />} />
       </Routes>
     </Router>
